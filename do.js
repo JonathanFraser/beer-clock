63e5dded19ff634fb2a066ff37c3a821313b5671
Jonathan Fraser <jonathan.fraser@altium.com>
2020-07-17 10:01:08 -0700

    added a drink message

--- do.js
+++ do.js
@@ -1,10 +1,15 @@
 
 function timeuntil(el) {
     var x = new Date()
-    var daysUntilFriday = (12 - x.getDay()) % 7
+    var day = x.getDay()
+    var daysUntilFriday = (12 - day) % 7
     x.setDate(daysUntilFriday + x.getDate())
     x.setHours(17, 0, 0, 0)
     duration = x - new Date()
+    if (duration < 0 || day === 0 || day === 6) {
+        el.innerHTML = "How about an IPA?"
+        return
+    }
     dur = new Date(duration)
     el.innerHTML = dur.toISOString().substr(11, 8)
 }
